19497c17ec062e02c19fe2c2290c53525da5a425
Fred6e6 <f2024ali22@gmail.com>
2025-12-26 17:05:17 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,39 @@
+const users = {
+  admin: { password: "600900", role: "admin" },
+
+  user2: { password: "102", role: "user" },
+  user3: { password: "103", role: "user" },
+  user4: { password: "104", role: "user" },
+  user5: { password: "123", role: "user" },
+  user6: { password: "106", role: "user" },
+  user7: { password: "107", role: "user" },
+  user8: { password: "108", role: "user" },
+  user9: { password: "109", role: "user" },
+  user10:{ password: "110", role: "user" }
+};
+
+function login() {
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const error = document.getElementById("error");
+
+  if (!users[username] || users[username].password !== password) {
+    error.innerText = "اسم المستخدم أو كلمة السر غير صحيحة";
+    return;
+  }
+
+  localStorage.setItem("user", username);
+  localStorage.setItem("role", users[username].role);
+
+  if (users[username].role === "admin") {
+    window.location.href = "admin.html";
+  } else {
+    window.location.href = "add.html";
+  }
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "index.html";
+}
 
